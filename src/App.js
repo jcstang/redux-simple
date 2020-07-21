0d0cd04fe4cd77f2b47d8ba0ab9ff175d104212f
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-20 22:07:48 -0600

    add github sign

--- src/App.js
+++ src/App.js
@@ -12,22 +12,22 @@ function App(props) {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{props.myReduxMessage}</p>
-        <button onClick={props.hiButtonPressed}>Say hi to Redux</button>
-        <h3>here is props: {props.myReduxNumber}</h3>
-        <button onClick={props.onIncrementCounter}>+</button>
-        <button onClick={props.onDecrementCounter}>-</button>
+        <button className="button" onClick={props.hiButtonPressed}>
+          Say hi to Redux
+        </button>
         <Counter />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Brought to you by &nbsp;
+          <a
+            className="App-link"
+            href="https://github.com/jcstang"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            jcstang
+          </a>
+          &nbsp; on GitHub
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
